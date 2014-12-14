91a0487812d47a3f629b232bc03e46be0d235067
Harz-FEAR <hartzfear@gmail.com>
2014-12-14 16:40:27 +0100

    fixed issue where search results from different search queries were mixed

--- resource/javascripts/controllers/search.js
+++ resource/javascripts/controllers/search.js
@@ -57,6 +57,9 @@ krusha.controller('searchCtrl', ['$scope', '$location', '$rootScope', 'apiSearch
 		var search_query = search_text.getText();
 
 		if (search_query !== null && search_query.length >= 2) {
+			$scope.shows = [];
+			$scope.shows_remote = [];
+			
 			apiSearch.searchLocal(search_query).then(function(data) {
 				$scope.shows = data.data.shows;
 			}, function() {
