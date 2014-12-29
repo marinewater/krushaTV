1f44c608d4266376f5864124963725d9adcb44c1
git <hartzfear@gmail.com>
2014-12-29 18:14:08 +0100

    switched to local mocha package

--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 SCRIPT_PATH=`pwd`/`dirname $0`
 ${SCRIPT_PATH}/../node_modules/karma/bin/karma start ${SCRIPT_PATH}/angular.test.js
-mocha ${SCRIPT_PATH}/mocha/
+${SCRIPT_PATH}/../node_modules/mocha/bin/mocha ${SCRIPT_PATH}/mocha/
