99f3ff83cab5ab7ce3b33982d707408f1ec8c99f
git <hartzfear@gmail.com>
2014-12-29 18:36:28 +0100

    added empty config files

---
+++ config/auth.js
@@ -0,0 +1,24 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '', // your App ID
+        'clientSecret'  : '', // your App Secret
+        'callbackURL'   : ''
+    },
+
+    'redditAuth' : {
+        'clientID'       : '',
+        'clientSecret'    : '',
+        'callbackURL'       : ''
+    },
+
+    'googleAuth' : {
+        'clientID'      : '',
+        'clientSecret'  : '',
+        'callbackURL'   : ''
+    }
+
+};
