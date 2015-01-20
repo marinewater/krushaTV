e23f306e0ef1cee1e51fc1c22713f1e7a4755e65
marinewater <HartzFEAR@gmail.com>
2015-01-20 03:24:03 +0100

    ability to keep being logged in for social auth

--- resource/javascripts/directives/loginButtons.js
+++ resource/javascripts/directives/loginButtons.js
@@ -1,6 +1,8 @@
 krusha.directive('loginButtons', function() {
     return {
         restrict: 'A',
-        template: '<li><a href="/auth/reddit" class="btn btn-default" target="_self"><i class="fa fa-reddit"></i> Reddit</a></li><li><a href="/auth/google" class="btn btn-danger" target="_self"><i class="fa fa-google"></i> Google</a></li><li><a href="/auth/facebook" class="btn btn-primary" target="_self"><i class="fa fa-facebook"></i> Facebook</a></li>'
+        template: '<li><a href="/auth/reddit?keep={{ keep_logged_in }}" class="btn btn-default" target="_self"><i class="fa fa-reddit"></i> Reddit</a></li>' +
+        '<li><a href="/auth/google?keep={{ keep_logged_in }}" class="btn btn-danger" target="_self"><i class="fa fa-google"></i> Google</a></li>' +
+        '<li><a ng-href="/auth/facebook?keep={{ keep_logged_in }}" class="btn btn-primary" target="_self"><i class="fa fa-facebook"></i> Facebook</a></li>'
     }
 });
